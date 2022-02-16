781ad4261e1939a467189f4a14e38c85242125fa
noman-swe <noman.swe130@gmail.com>
2022-02-17 02:11:43 +0600

    28-6 Use event bubble to create calculator and clear

--- js/pin.js
+++ js/pin.js
@@ -14,3 +14,21 @@ function generatePin(){
   
   document.getElementById('display-pin').value = pin;
 }
+
+
+// 
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if(isNaN(number)){
+        if(number == 'C'){
+calcInput.value = '';
+        }
+    }else{
+        
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number; 
+        calcInput.value = newNumber;
+    }
+    
+})
